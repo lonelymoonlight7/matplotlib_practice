9e1ec90ef851ddc5d6c2c7de0c95b8d75b1d7838
lonelymoonlight7 <2523742849@qq.com>
2023-07-17 16:35:16 +0800

    first commit

---
+++ python_repos.py
@@ -0,0 +1,29 @@
+import requests
+
+#执行API调用并查看响应
+url = "https://api.github.com/search/repositories"
+url += "?q=language:python+sort:stars+stars:>10000"
+
+headers = {"Accept": "application/vnd.github.v3+json"}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+#将响应转化为字典
+response_dict = r.json()
+print(f"Total respositories: {response_dict['total_count']}")
+print(f"Complete results: {not response_dict['incomplete_results']}")
+
+#探索有关仓库的信息
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print("\nSelected information about first repository:")
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
